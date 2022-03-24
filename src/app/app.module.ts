0774c25281d31e90be7dd00d0f33886f27106b64
Ezequiel Maureira <ezequiel.maureira@gmail.com>
2022-03-24 13:22:02 -0300

    CreaciondeComponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentesFuncionales/header/header.component';
+import { AcercaComponent } from './componentesFuncionales/acerca/acerca.component';
+import { ExperienciaComponent } from './componentesFuncionales/experiencia/experiencia.component';
+import { EducacionComponent } from './componentesFuncionales/educacion/educacion.component';
+import { CompetenciasComponent } from './componentesFuncionales/competencias/competencias.component';
+import { ProyectosComponent } from './componentesFuncionales/proyectos/proyectos.component';
+import { VistaComponent } from './componenteVista/vista/vista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaComponent,
+    HeaderComponent,
+    AcercaComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    CompetenciasComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
