fe986790e9c6feadac5f38de093ce08626065a9b
Ezequiel Maureira <ezequiel.maureira@gmail.com>
2022-03-26 13:38:14 -0300

    a

--- src/app/porfolio.service.ts
+++ src/app/porfolio.service.ts
@@ -10,13 +10,15 @@ import { AcercaComponent } from './componentesFuncionales/acerca/acerca.componen
 export class PorfolioService {
   
   
-  private ApiUrl='http://localhost:4000/acercaDe';
+  private ApiUrlNombre='http://localhost:4000/Nombre';
+  private ApiUrlEspecialidad='http://localhost:4000/Especialidad';
+  private ApiUrlDescripcion='http://localhost:4000/Descripcion';
   constructor(private http:HttpClient) { }
 
  
   obtenerAcercaDe():Observable<Ifnombre>{
 
-    return this.http.get<Ifnombre>(this.ApiUrl)
+    return this.http.get<Ifnombre>(this.ApiUrlNombre)
     
       
     }
@@ -24,17 +26,17 @@ export class PorfolioService {
     
   actualizarNombre(nombre:any, id:number):Observable<any>{
    
-    return this.http.put<any>(this.ApiUrl+"/"+id,nombre) 
+    return this.http.put<any>(this.ApiUrlNombre+"/"+id,nombre) 
 }
   
 actualizarEspecialidad(especialidad:any, id:number):Observable<any>{
    
-  return this.http.put<any>(this.ApiUrl+"/"+id,especialidad)
+  return this.http.put<any>(this.ApiUrlEspecialidad+"/"+id,especialidad)
 
 }
 actualizarDescripcion(descripcion:any, id:number):Observable<any>{
    
-  return this.http.put<any>(this.ApiUrl+"/"+id,descripcion)
+  return this.http.put<any>(this.ApiUrlDescripcion+"/"+id,descripcion)
 
 }
 
