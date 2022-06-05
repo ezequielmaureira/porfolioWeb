48a665e0930ad59c37b649ec9dffba9d97397458
Ezequiel Maureira <ezequiel.maureira@gmail.com>
2022-06-05 19:42:15 -0300

    s

---
+++ src/app/componentesFuncionales/educacion/educacionModel.ts
@@ -0,0 +1,13 @@
+export class eduModel {
+
+    id:number=0;
+    inicio:number=0;
+    fin:number=0;
+    instituccion:string="";
+    titulo:string="";
+  institucion: any;
+  
+    
+    
+    }
+    
